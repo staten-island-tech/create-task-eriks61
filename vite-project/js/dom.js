1478034c7171ee99e6de97c0f7aac0344792f78e
eriks61 <eriks61@nycstudents.net>
2024-01-16 09:16:09 -0500

    lkjhb

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,6 @@
 const DOMselectors = {
   quote: document.getElementById("quote"),
+  like: document.getElementById("btn1"), 
+  dislike:  document.getElementById("btn2"),
 }
 export {DOMselectors};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,19 +1,26 @@
-import './style.css'
+
 import { DOMselectors } from './dom.js';
 
 const url = "https://api.quotable.io/random";
 
 async function createCard(){
+  DOMselectors.quote.innerHTML=""
   try {
     const response = await fetch(url);
     if(!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-    DOMselectors.quote.insertAdjacentHTML("beforeend",  
+    DOMselectors.quote.insertAdjacentHTML("beforeend", 
+    `<div id="quote"
+    <h2 class="card-name">${data.content}</h2>
+    </div>`
     )
   }
-  catch {
-
+  catch (error){
+  console.error("error", error);
   }
 }
+createCard();
+DOMselectors.dislike.addEventListener("click", 
+createCard)
