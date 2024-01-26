ff609d39f330109b572056138b90de344879c309
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-01-26 17:52:45 -0500

    b n

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,7 +4,30 @@ import { DOMselectors } from './dom.js';
 const url = "https://api.quotable.io/random";
 let likedQuotes = [];
 
-async function createCard(){
+async function createCardlike(){
+  DOMselectors.quote.innerHTML=""
+  try {
+    const response = await fetch(url);
+    if(!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    DOMselectors.quote.insertAdjacentHTML("beforeend", 
+    `<div id="bird"
+    <h2 class="card-name">${data.content}</h2>
+    </div>`
+    )
+    const quoteContent = DOMselectors.quote.textContent;
+    if (quoteContent) {
+      likedQuotes.push(quoteContent);
+      console.log(likedQuotes);
+    }
+  }
+  catch (error){
+  console.error("error", error);
+  }
+}
+async function createCardDislike(){
   DOMselectors.quote.innerHTML=""
   try {
     const response = await fetch(url);
@@ -23,39 +46,12 @@ async function createCard(){
   }
 }
 
-
-async function store(){
-  try {
-    const response = await fetch(url);
-    if(!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-
- 
-}
-catch (error){
-  console.error("error", error);
-}
-}
-
-function addToArray(){
-  let likedQuotes = [];
-  const quoteContent = DOMselectors.quote.textContent;
-  if (quoteContent) {
-    likedQuotes.push(quoteContent);
-    console.log(likedQuotes);
-  }
   
-}
+
 
 DOMselectors.dislike.addEventListener("click", 
-createCard)
+createCardDislike)
 
-DOMselectors.like.addEventListener("click", function () {
-  console.log('clicked')
-  createCard();
-  addToArray();
-}
-)
-createCard()
+DOMselectors.like.addEventListener("click", 
+  createCardlike)
+
