e86a2d3d7682d80cacc243d7b800be387efc84d2
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-01-27 10:09:47 -0500

    ac

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,8 +3,7 @@ import { DOMselectors } from './dom.js';
 
 const url = "https://api.quotable.io/random";
 let likedQuotes = [];
-
-async function createCardlike(){
+async function createCard(){
   DOMselectors.quote.innerHTML=""
   try {
     const response = await fetch(url);
@@ -17,36 +16,35 @@ async function createCardlike(){
     <h2 class="card-name">${data.content}</h2>
     </div>`
     )
+  }
+  catch (error){
+  console.error("error", error);
+  }
+}
+async function createCardlike(){
+  createCard()
+  
     const quoteContent = DOMselectors.quote.textContent;
     if (quoteContent) {
       likedQuotes.push(quoteContent);
       console.log(likedQuotes);
     }
-  }
-  catch (error){
-  console.error("error", error);
-  }
+
+
 }
 async function createCardDislike(){
-  DOMselectors.quote.innerHTML=""
-  try {
-    const response = await fetch(url);
-    if(!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    DOMselectors.quote.insertAdjacentHTML("beforeend", 
-    `<div id="bird"
-    <h2 class="card-name">${data.content}</h2>
-    </div>`
-    )
-  }
-  catch (error){
-  console.error("error", error);
-  }
+  createCard();
 }
 
-  
+function showQuotes(){
+  DOMselectors.quote.innerHTML = "";
+  likedQuotes.forEach((quote, index) => {
+    DOMselectors.insertAdjacentHTML("beforeend", 
+    `<div class="boxy">
+    <p>${index + 1}. ${quote}</p>
+    </div>`)
+  })
+}  
 
 
 DOMselectors.dislike.addEventListener("click", 
@@ -54,4 +52,7 @@ createCardDislike)
 
 DOMselectors.like.addEventListener("click", 
   createCardlike)
+
+DOMselectors.myQuotes.addEventListener("click", 
+showQuotes)
 
